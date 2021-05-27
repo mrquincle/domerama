346b45033fbdfaaa1c447af05c627c488dc012bc
Anne van Rossum <anne@crownstone.rocks>
2021-05-27 22:28:06 +0200

    Use domerama.nl

--- xmlrpc.php
+++ xmlrpc.php
@@ -2,12 +2,12 @@
   <service>
     <engineName>WordPress</engineName>
     <engineLink>http://wordpress.org/</engineLink>
-    <homePageLink>http://www.domerama.com</homePageLink>
+    <homePageLink>http://www.domerama.nl</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="http://www.domerama.com/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://www.domerama.com/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://www.domerama.com/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="http://www.domerama.com/xmlrpc.php" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://www.domerama.nl/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://www.domerama.nl/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://www.domerama.nl/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://www.domerama.nl/xmlrpc.php" />
           </apis>
   </service>
 </rsd>
